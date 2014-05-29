0a6401957ac25f44d446cc3c34cb78a8f42b0332
Ramesh Elaiyavalli <rameshe@wadzeemac.local>
2014-05-29 10:35:29 -0700

    Cleanup
    Removed ogr.bat, validate.py and VectorPrompt.lnk

--- ogr.bat
+++
@@ -1,8 +0,0 @@
-ECHO Setting environment for using GDAL/OGR and Python.
-SET ORIGPATH=%PATH%
-SET PATH=B:\Program Files (x86)\FWTools2.4.7\bin;%PATH%
-SET GDAL_DATA=B:\Program Files (x86)\FWTools2.4.7\data
-SET S57_CSV=B:\Program Files (x86)\FWTools2.4.7\data
-SET PGCLIENTENCODING=LATIN1
-
-ogr2ogr -update -append -gt "65536" -f "PostgreSQL" PG:"dbname='PyTest' host='127.0.0.1' port='5432' user='postgres' password='postgres'" %1
